aa03978e401b7444c7daa526caa95875e7020870
Richa Mehta <richamehta1197@gmail.com>
2017-09-25 13:46:31 +0530

    first commit

---
+++ reverse-mad-libs.py
@@ -0,0 +1,34 @@
+# Question for first_level 
+first_level = """__1__ is a performing art form consisting of purposefully selected sequences of __3__ movement.
+ This movement has aesthetic and symbolic __2__, and is acknowledged as dance by performers and observers within a particular culture.
+  Dance can be categorized and described by its __4__,
+ by its repertoire of movements, or by its historical period or __5__ of origin."""
+
+# Answer for first_level
+first_level_answers = ['Dance', 'value', 'human', 'choreography', 'place']
+
+
+# Question for second_level
+second_level ="""__1__ shopping is a form of electronic commerce which allows __2__ to directly buy goods or services from a seller over the Internet using a web browser. 
+Consumers find a product of interest by visiting the __3__ of the retailer directly or by searching among alternative vendors using a shopping search engine, which displays the same product's availability.
+ As of 2016, customers can __4__ online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
+
+# Answer for second_level 
+level_2_answers = ['online', 'consumers', 'website', 'shop', 'range']
+
+
+
+# Question string for third_level 
+level_3 ="""A 100 gram serving of milk __1__ supplies 540 __2__.
+ It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the __3__ in milk chocolate is saturated,
+  composed mainly of palmitic __4__ and stearic acid, while the predominant unsaturated fat is __5__ acid."""
+
+# Answer for third_level 
+level_3_answers = ['chocolate', 'calories', 'fat', 'acid', 'oleic']
+
+
+
+
+
+
+
