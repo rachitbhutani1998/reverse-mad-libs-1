2b167028ee787e222ff16c3d5a03494b8c7bab6b
Richa Mehta <richamehta1197@gmail.com>
2017-09-29 22:18:10 +0530

    updated code

--- reverse-mad-libs.py
+++ reverse-mad-libs.py
@@ -1,39 +1,8 @@
 # Question for first_level 
 first_level = """__1__ is a performing art form consisting of purposefully selected sequences of __2__ movement.
- This movement has aesthetic and symbolic __3__, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its __4__,
+ This movement has aesthetic and symbolic __3__ , and is acknowledged as dance by performers and observers within a particular culture.
+  Dance can be categorized and described by its __4__ ,
  by its repertoire of movements, or by its historical period or __5__ of origin."""
-
-
-
-def first_level_1(blank):
-    s="""dance is a performing art form consisting of purposefully selected sequences of __2__ movement.
- This movement has aesthetic and symbolic __3__, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its __4__,
- by its repertoire of movements, or by its historical period or __5__ of origin."""
-    print s
-def first_level_2(blank):
-    print """dance is a performing art form consisting of purposefully selected sequences of value movement.
- This movement has aesthetic and symbolic __3__, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its __4__,
- by its repertoire of movements, or by its historical period or __5__ of origin."""
-def first_level_3(blank):
-    print """dance is a performing art form consisting of purposefully selected sequences of value movement.
- This movement has aesthetic and symbolic human, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its __4__,
- by its repertoire of movements, or by its historical period or __5__ of origin."""
-def first_level_4(blank):
-    print """dance is a performing art form consisting of purposefully selected sequences of value movement.
- This movement has aesthetic and symbolic human, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its choreography,
- by its repertoire of movements, or by its historical period or __5__ of origin."""
-def first_level_5(blank):
-    print """dance is a performing art form consisting of purposefully selected sequences of value movement.
- This movement has aesthetic and symbolic human, and is acknowledged as dance by performers and observers within a particular culture.
-  Dance can be categorized and described by its choreography,
- by its repertoire of movements, or by its historical period or place of origin."""
-    
-
 # Answer for first_level
 first_level_answers = ['dance', 'value', 'human', 'choreography', 'place']
 
@@ -43,73 +12,17 @@ second_level ="""__1__ shopping is a form of electronic commerce which allows __
 Consumers find a product of interest by visiting the __3__ of the retailer directly or by searching among alternative vendors using a shopping search engine,
  which displays the same product's availability.
  As of 2016, customers can __4__ online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-
-
-
-
-def second_level_1(blank):
-   print  """online shopping is a form of electronic commerce which allows __2__ to directly buy goods or services from a seller over the Internet using a web browser. 
-Consumers find a product of interest by visiting the __3__ of the retailer directly or by searching among alternative vendors using a shopping search engine,
- which displays the same product's availability.
- As of 2016, customers can __4__ online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-def second_level_2(blank):
-   print  """online shopping is a form of electronic commerce which allows consumers to directly buy goods or services from a seller over the Internet using a web browser. 
-Consumers find a product of interest by visiting the __3__ of the retailer directly or by searching among alternative vendors using a shopping search engine, 
-which displays the same product's availability.
- As of 2016, customers can __4__ online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-def second_level_3(blank):
-   print  """online shopping is a form of electronic commerce which allows value to directly buy goods or services from a seller over the Internet using a web browser. 
-Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, 
-which displays the same product's availability.
- As of 2016, customers can __4__ online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-def second_level_4(blank):
-   print  """online shopping is a form of electronic commerce which allows value to directly buy goods or services from a seller over the Internet using a web browser. 
-Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, 
-which displays the same product's availability.
- As of 2016, customers can shop online using a __5__ of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-def second_level_5(blank):
-   print  """online shopping is a form of electronic commerce which allows value to directly buy goods or services from a seller over the Internet using a web browser. 
-Consumers find a product of interest by visiting the website of the retailer directly or by searching among alternative vendors using a shopping search engine, 
-which displays the same product's availability.
- As of 2016, customers can shop online using a range of different computers and devices, including desktop computers, laptops, tablet computers and smartphones."""
-
 # Answer for second_level 
 second_level_answers = ['online', 'consumers', 'website', 'shop', 'range']
 
 
 
 # Question string for third_level 
-level_3 ="""A 100 gram serving of milk __1__ supplies 540 __2__.
+third_level ="""A 100 gram serving of milk __1__ supplies 540 __2__.
  It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the __3__ in milk chocolate is saturated,
   composed mainly of palmitic __4__ and stearic acid, while the predominant unsaturated fat is __5__ acid."""
-
-
-
-def third_level_1(blank):
-    print """A 100 gram serving of milk chocolate supplies 540 __2__.
- It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the __3__ in milk chocolate is saturated,
-  composed mainly of palmitic __4__ and stearic acid, while the predominant unsaturated fat is __5__ acid."""
-def third_level_2(blank):
-    print """A 100 gram serving of milk chocolate supplies 540 calories.
- It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the __3__ in milk chocolate is saturated,
-  composed mainly of palmitic __4__ and stearic acid, while the predominant unsaturated fat is __5__ acid."""
-def third_level_3(blank):
-    print """A 100 gram serving of milk chocolate supplies 540 calories.
- It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the fat in milk chocolate is saturated,
-  composed mainly of palmitic __4__ and stearic acid, while the predominant unsaturated fat is __5__ acid."""
-def third_level_4(blank):
-    print """A 100 gram serving of milk chocolate supplies 540 calories.
- It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the fat in milk chocolate is saturated,
-  composed mainly of palmitic acid and stearic acid, while the predominant unsaturated fat is __5__ acid."""
-def third_level_5(blank):
-    print """A 100 gram serving of milk chocolate supplies 540 calories.
- It is 59% carbohydrates , 30% fat and 8% protein .Approximately 65% of the fat in milk chocolate is saturated,
-  composed mainly of palmitic acid and stearic acid, while the predominant unsaturated fat is oleic acid."""
-
 # Answer for third_level 
 third_level_answers = ['chocolate', 'calories', 'fat', 'acid', 'oleic']
-
-
 
 
 
@@ -118,45 +31,73 @@ def levels():
 The function has no input .This function asks the user for hardness of the level and then returns the level asked by user.
 '''
 
-    level = raw_input('''Enter easy for first_level medium for second_level hard for third_level''')
+    level = raw_input('''Enter easy for first_level medium for second_level hard for third_level \n''')
     if level == 'easy':
-        print first_level
-        start_game(first_level, first_level_answers)
+        print str(first_level)
+        game_play(first_level, first_level_answers)
     elif level == 'medium':
-        print _second_level
-        start_game(second_level, second_level_answers)
+        print str(second_level)
+        game_play(second_level, second_level_answers)
     elif level == 'hard':
         print str(third_level)
-        start_game(third_level, third_level_answers)
+        game_play(third_level, third_level_answers)
     else:
         print 'You entered a wrong choice\n'
         levels()
 
 
-def start_game(level_ques,level_ans):
+
+def game_play(level_ques,level_ans):
     count=0
+    number=4
+    question = level_ques
     while count<len(level_ans):
+        level_ques = question
         answer = raw_input('What is the answer for blank __'
                            + str(count + 1) + '__ ?\n')
-        if correct_ans(answer,level_ans):
-            first_level_1(answer)
-        count=count+1
-            
-         
-         
+        if correct_ans(count,level_ans,answer):
+           question = replacement(level_ques,level_ans,count,answer)
+           print question
+           count += 1
+           
+           print("\n Congrats!! It is a  right answer! \n")
+        
+        else:
+           number -= 1 
 
-def correct_ans(user_answer,actual_answer):
-    if user_answer == actual_answer:
-        return 1
-
-
-    
-
-levels()
-
-
-
+           #it reduces the number of attempts after a wrong answer
+           
+           print("\n It is a wrong answer...Try again, " + str(number) +" attempts remaining\n")
+           if number == 0:
+            break
+ 
 
 
 
 
+def correct_ans(count,level_ans,answer ):
+    if answer == level_ans[count]:
+        return True
+    else:
+        return False
+    '''
+....This function takes the answer entered by the user and the correct answer from the given list
+ .... and returns a value which is either true or fase
+....'''
+def replacement(level_ques,level_ans,count,answer):
+    level_ques = level_ques.split()
+
+        # index is used to store the index of the first blank in the question string
+
+    index = level_ques.index('__' + str(count + 1) + '__')
+    level_ques[index] = level_ques[index].replace('__' + str(count
+            + 1) + '__', answer)
+    level_ques = ' '.join(level_ques)
+    #it returns the level_ques with the corrected answer
+    return level_ques
+
+
+
+
+
+levels()    
